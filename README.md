20b51b36ccac6e6c97f8abd8c125e57679a2001c
jaime-pc <jaimecr9@gmail.com>
2020-09-25 18:54:14 +0200

    Creada la estructura básica del proyecto.

---
+++ README.md
@@ -0,0 +1,2 @@
+# ml-fingerprint
+Capa de ciberseguridad sobre el paquete de Machine Learning scikit-learn.
---
+++ setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+
+setup(
+   name='ml-fingerprint',
+   version='0',
+   description='Capa de ciberseguridad sobre el paquete de Machine Learning scikit-learn',
+   author='Jaime Solsona',
+   author_email='j.solsonaa@alumnos.urjc.es',
+   packages=['ml-fingerprint'],
+   install_requires=['scikit-learn'],
+)
